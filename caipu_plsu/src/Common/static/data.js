47ab9c406c5681c4d15d519216e11ba1ff61e3d2
koala0521 <328762691@qq.com>
2019-12-09 18:33:00 +0800

    静态数据配置

---
+++ caipu_plsu/src/Common/static/data.js
@@ -0,0 +1,447 @@
+export default {
+    "classid": 301,
+    "name": "菜品",
+    "parentid": 0,
+    "list": [
+        {
+            "classid": 302,
+            "name": "家常菜",
+            "parentid": 301
+        },
+        {
+            "classid": 303,
+            "name": "私房菜",
+            "parentid": 301
+        },
+        {
+            "classid": 304,
+            "name": "快手菜",
+            "parentid": 301
+        },
+        {
+            "classid": 305,
+            "name": "热菜",
+            "parentid": 301
+        },
+        {
+            "classid": 306,
+            "name": "凉菜",
+            "parentid": 301
+        },
+        {
+            "classid": 307,
+            "name": "素菜",
+            "parentid": 301
+        },
+        {
+            "classid": 308,
+            "name": "蒸菜",
+            "parentid": 301
+        },
+        {
+            "classid": 309,
+            "name": "创意菜",
+            "parentid": 301
+        },
+        {
+            "classid": 310,
+            "name": "下饭菜",
+            "parentid": 301
+        },
+        {
+            "classid": 311,
+            "name": "下酒菜",
+            "parentid": 301
+        },
+        {
+            "classid": 312,
+            "name": "海鲜",
+            "parentid": 301
+        },
+        {
+            "classid": 313,
+            "name": "药膳",
+            "parentid": 301
+        },
+        {
+            "classid": 314,
+            "name": "斋菜",
+            "parentid": 301
+        },
+        {
+            "classid": 315,
+            "name": "卤菜",
+            "parentid": 301
+        },
+        {
+            "classid": 316,
+            "name": "腌菜",
+            "parentid": 301
+        },
+        {
+            "classid": 317,
+            "name": "农家菜",
+            "parentid": 301
+        },
+        {
+            "classid": 318,
+            "name": "宫廷菜",
+            "parentid": 301
+        },
+        {
+            "classid": 319,
+            "name": "婴儿辅食",
+            "parentid": 301
+        },
+        {
+            "classid": 320,
+            "name": "饭",
+            "parentid": 301
+        },
+        {
+            "classid": 321,
+            "name": "炒饭",
+            "parentid": 301
+        },
+        {
+            "classid": 322,
+            "name": "盖浇饭",
+            "parentid": 301
+        },
+        {
+            "classid": 323,
+            "name": "煲仔饭",
+            "parentid": 301
+        },
+        {
+            "classid": 324,
+            "name": "粥",
+            "parentid": 301
+        },
+        {
+            "classid": 325,
+            "name": "面食",
+            "parentid": 301
+        },
+        {
+            "classid": 326,
+            "name": "面",
+            "parentid": 301
+        },
+        {
+            "classid": 327,
+            "name": "炒面",
+            "parentid": 301
+        },
+        {
+            "classid": 328,
+            "name": "粉",
+            "parentid": 301
+        },
+        {
+            "classid": 329,
+            "name": "饼",
+            "parentid": 301
+        },
+        {
+            "classid": 330,
+            "name": "油酥类面点",
+            "parentid": 301
+        },
+        {
+            "classid": 331,
+            "name": "糕点",
+            "parentid": 301
+        },
+        {
+            "classid": 332,
+            "name": "汤圆",
+            "parentid": 301
+        },
+        {
+            "classid": 333,
+            "name": "馄饨",
+            "parentid": 301
+        },
+        {
+            "classid": 334,
+            "name": "馒头",
+            "parentid": 301
+        },
+        {
+            "classid": 335,
+            "name": "饺子",
+            "parentid": 301
+        },
+        {
+            "classid": 336,
+            "name": "包子",
+            "parentid": 301
+        },
+        {
+            "classid": 337,
+            "name": "卷子",
+            "parentid": 301
+        },
+        {
+            "classid": 338,
+            "name": "烘焙",
+            "parentid": 301
+        },
+        {
+            "classid": 339,
+            "name": "蛋糕",
+            "parentid": 301
+        },
+        {
+            "classid": 340,
+            "name": "面包",
+            "parentid": 301
+        },
+        {
+            "classid": 341,
+            "name": "饼干",
+            "parentid": 301
+        },
+        {
+            "classid": 342,
+            "name": "披萨",
+            "parentid": 301
+        },
+        {
+            "classid": 343,
+            "name": "派类",
+            "parentid": 301
+        },
+        {
+            "classid": 344,
+            "name": "挞类",
+            "parentid": 301
+        },
+        {
+            "classid": 345,
+            "name": "果冻",
+            "parentid": 301
+        },
+        {
+            "classid": 346,
+            "name": "布丁",
+            "parentid": 301
+        },
+        {
+            "classid": 347,
+            "name": "冰淇淋",
+            "parentid": 301
+        },
+        {
+            "classid": 348,
+            "name": "糖果",
+            "parentid": 301
+        },
+        {
+            "classid": 349,
+            "name": "点心",
+            "parentid": 301
+        },
+        {
+            "classid": 350,
+            "name": "零食",
+            "parentid": 301
+        },
+        {
+            "classid": 351,
+            "name": "汤",
+            "parentid": 301
+        },
+        {
+            "classid": 352,
+            "name": "羹",
+            "parentid": 301
+        },
+        {
+            "classid": 353,
+            "name": "炖品",
+            "parentid": 301
+        },
+        {
+            "classid": 354,
+            "name": "甜品",
+            "parentid": 301
+        },
+        {
+            "classid": 355,
+            "name": "饮品",
+            "parentid": 301
+        },
+        {
+            "classid": 356,
+            "name": "冰品",
+            "parentid": 301
+        },
+        {
+            "classid": 357,
+            "name": "糖水",
+            "parentid": 301
+        },
+        {
+            "classid": 358,
+            "name": "沙拉",
+            "parentid": 301
+        },
+        {
+            "classid": 359,
+            "name": "茶饮",
+            "parentid": 301
+        },
+        {
+            "classid": 360,
+            "name": "酒水",
+            "parentid": 301
+        },
+        {
+            "classid": 361,
+            "name": "花草茶",
+            "parentid": 301
+        },
+        {
+            "classid": 362,
+            "name": "果汁",
+            "parentid": 301
+        },
+        {
+            "classid": 363,
+            "name": "小吃",
+            "parentid": 301
+        },
+        {
+            "classid": 364,
+            "name": "便当",
+            "parentid": 301
+        },
+        {
+            "classid": 365,
+            "name": "烧烤",
+            "parentid": 301
+        },
+        {
+            "classid": 366,
+            "name": "串烧",
+            "parentid": 301
+        },
+        {
+            "classid": 367,
+            "name": "寿司",
+            "parentid": 301
+        },
+        {
+            "classid": 368,
+            "name": "拼盘",
+            "parentid": 301
+        },
+        {
+            "classid": 369,
+            "name": "杂烩",
+            "parentid": 301
+        },
+        {
+            "classid": 370,
+            "name": "酱汁",
+            "parentid": 301
+        },
+        {
+            "classid": 371,
+            "name": "果酱",
+            "parentid": 301
+        },
+        {
+            "classid": 372,
+            "name": "火锅",
+            "parentid": 301
+        },
+        {
+            "classid": 373,
+            "name": "干锅",
+            "parentid": 301
+        },
+        {
+            "classid": 374,
+            "name": "香锅",
+            "parentid": 301
+        },
+        {
+            "classid": 375,
+            "name": "佐料",
+            "parentid": 301
+        },
+        {
+            "classid": 376,
+            "name": "自制调味料",
+            "parentid": 301
+        },
+        {
+            "classid": 377,
+            "name": "懒人食谱",
+            "parentid": 301
+        },
+        {
+            "classid": 378,
+            "name": "包子馅",
+            "parentid": 301
+        },
+        {
+            "classid": 379,
+            "name": "馄饨馅",
+            "parentid": 301
+        },
+        {
+            "classid": 380,
+            "name": "饺子馅",
+            "parentid": 301
+        },
+        {
+            "classid": 381,
+            "name": "水饺馅",
+            "parentid": 301
+        },
+        {
+            "classid": 382,
+            "name": "粽子",
+            "parentid": 301
+        },
+        {
+            "classid": 383,
+            "name": "月饼",
+            "parentid": 301
+        },
+        {
+            "classid": 384,
+            "name": "月饼馅",
+            "parentid": 301
+        },
+        {
+            "classid": 385,
+            "name": "果茶",
+            "parentid": 301
+        },
+        {
+            "classid": 386,
+            "name": "石锅拌饭",
+            "parentid": 301
+        },
+        {
+            "classid": 387,
+            "name": "谭家菜",
+            "parentid": 301
+        },
+        {
+            "classid": 388,
+            "name": "官府菜",
+            "parentid": 301
+        },
+        {
+            "classid": 389,
+            "name": "孔府菜",
+            "parentid": 301
+        }
+    ]
+}
